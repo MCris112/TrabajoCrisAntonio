6a9095755b835652aeccf5f79d8579959415f2f9
antoolr <antoniolozanorodriguez13@gmail.com>
2026-03-05 09:01:28 +0100

    modelo

---
+++ src/Modelos/libro.java
@@ -0,0 +1,12 @@
+package Modelos;
+
+import com.darkredgm.querymc.Annotations.DBColPrimary;
+import com.darkredgm.querymc.Annotations.DbColumn;
+
+public class libro {
+    @DBColPrimary
+    @DbColumn
+    private int id;
+
+    
+}
