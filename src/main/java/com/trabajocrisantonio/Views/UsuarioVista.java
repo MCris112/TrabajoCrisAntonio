b6b667387abf7866c49620193653bdb7e3156d55
antoolr <antoniolozanorodriguez13@gmail.com>
2026-03-09 13:49:57 +0100

    vista usuario terminada

--- src/main/java/com/trabajocrisantonio/Views/UsuarioVista.java
+++ src/main/java/com/trabajocrisantonio/Views/UsuarioVista.java
@@ -3,7 +3,9 @@ package com.trabajocrisantonio.Views;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
@@ -28,11 +30,36 @@ public class UsuarioVista extends JPanel {
     public UsuarioVista(){
     setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
-    panelFormulario.setBorder(BorderFactory.createTitledBorder("Datos Categoría"));
+    panelFormulario.setBorder(BorderFactory.createTitledBorder("Datos Usuario"));
 
+    JPanel panelBtn = new JPanel();
+    panelBtn.add(btnInsertar);
+    panelBtn.add(btnBorrar);
+    panelBtn.add(btnActualizar);
+    panelBtn.add(btnLimpiar);
+
+    JScrollPane scroll = new JScrollPane(table);
+    scroll.setBorder(BorderFactory.createTitledBorder("Listado usuarios"));
+
+    panelFormulario.add(new JLabel("nif"));
+    panelFormulario.add(fieldNif);
+
+    panelFormulario.add(new JLabel("nombre"));
+    panelFormulario.add(fieldNombre);
+
+    panelFormulario.add(new JLabel("apellidos"));
+    panelFormulario.add(fieldApellido);
+
+    panelFormulario.add(new JLabel("telefono"));
+    panelFormulario.add(fieldTelefono);
+
+    panelFormulario.add(new JLabel("direccion"));
+    panelFormulario.add(fieldDireccion);
+
+    add(panelBtn);
     add(panelFormulario);
-    }
+    add(scroll);
 
-    
+    }  
 
 }
