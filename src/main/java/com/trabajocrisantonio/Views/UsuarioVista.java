7e670ebd0e8a094522a1e64e86e6e29d8ee65e04
antoolr <antoniolozanorodriguez13@gmail.com>
2026-03-09 13:53:07 +0100

    apellidos

--- src/main/java/com/trabajocrisantonio/Views/UsuarioVista.java
+++ src/main/java/com/trabajocrisantonio/Views/UsuarioVista.java
@@ -16,11 +16,11 @@ public class UsuarioVista extends JPanel {
     public JButton btnBorrar = new JButton("Borrar");
     public JButton btnLimpiar = new JButton("Limpiar");
 
-    public JTextField fieldNif = new JTextField();
-    public JTextField fieldNombre = new JTextField();
-    public JTextField fieldApellido = new JTextField();
-    public JTextField fieldDireccion = new JTextField();
-    public JTextField fieldTelefono = new JTextField();
+    public JTextField fieldNif = new JTextField(6);
+    public JTextField fieldNombre = new JTextField(6);
+    public JTextField fieldApellido = new JTextField(9);
+    public JTextField fieldDireccion = new JTextField(6);
+    public JTextField fieldTelefono = new JTextField(6);
 
     public DefaultTableModel modeloTabla = new DefaultTableModel( new Object[]{"nif", "nombre", "apellidos", "direccion", "telefono"}, 0 );
 
@@ -32,15 +32,18 @@ public class UsuarioVista extends JPanel {
 
     panelFormulario.setBorder(BorderFactory.createTitledBorder("Datos Usuario"));
 
+    // Panel donde se van a ver todos los botones creados
     JPanel panelBtn = new JPanel();
     panelBtn.add(btnInsertar);
     panelBtn.add(btnBorrar);
     panelBtn.add(btnActualizar);
     panelBtn.add(btnLimpiar);
 
+    //Contenedor donde se ven los datos de los usuarios
     JScrollPane scroll = new JScrollPane(table);
     scroll.setBorder(BorderFactory.createTitledBorder("Listado usuarios"));
 
+    //Linea datos usuario, los campos que pertenecen a usuario
     panelFormulario.add(new JLabel("nif"));
     panelFormulario.add(fieldNif);
 
