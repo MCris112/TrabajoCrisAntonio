6126d50315755dce21bca33a15d1b7404344b931
antoolr <antoniolozanorodriguez13@gmail.com>
2026-03-09 12:41:46 +0100

    asfasdf

--- src/Modelos/libro.java
+++ src/Modelos/libro.java
@@ -6,7 +6,27 @@ import com.darkredgm.querymc.Annotations.DbColumn;
 public class libro {
     @DBColPrimary
     @DbColumn
-    private int id;
+    private int id_libro;
 
-    
+    @DbColumn
+    private String editorial;
+
+    @DbColumn
+    private int numero_hojas;
+
+    @DbColumn
+    private String titulo;
+
+    @DbColumn
+    private String autor;
+
+    @DbColumn
+    private String genero;
+
+    @DbColumn
+    private double precio;
+
+    @DbColumn
+    private boolean bestseller;
+
 }
